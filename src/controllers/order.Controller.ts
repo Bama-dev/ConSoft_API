a9ec7f809c97c5e2d795b78d36a326952c85b1b7
Sant15123 <sansuadur2006@gmail.com>
2025-09-19 10:42:06 -0500

    fix(controller): arreglar populate de usuarios

--- src/controllers/order.Controller.ts
+++ src/controllers/order.Controller.ts
@@ -1,18 +1,19 @@
-import { Request, Response } from 'express';
-import { OrderModel } from '../models/order.model';
-import { createCrudController } from './crud.controller';
+import { Request, Response } from "express";
+import { OrderModel } from "../models/order.model";
+import { createCrudController } from "./crud.controller";
 
 const base = createCrudController(OrderModel);
 
 export const OrderController = {
-	...base,
-	list: async (req: Request, res: Response) => {
-		try {
-			const orders = await OrderModel.find().populate('user').select("-password -__v");
-			res.json(orders);
-		} catch (error) {
-			res.status(500).json({ message: 'Error retrieving orders' });
-		}
-	}
-
+  ...base,
+  list: async (req: Request, res: Response) => {
+    try {
+      const orders = await OrderModel.find()
+        .populate("user", "-password -__v ")
+        .populate("payments");
+      res.json(orders);
+    } catch (error) {
+      res.status(500).json({ message: "Error retrieving orders" });
+    }
+  },
 };
