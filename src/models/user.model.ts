9161b1f6abfdaa74f1f5b8c50c4c1789c88b8cd3
Sant15123 <sansuadur2006@gmail.com>
2025-09-19 08:15:39 -0500

    fix(models): arreglar modelo de usuarios

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,7 +4,7 @@ import { IUser } from '../types/interfaces';
 const UserSchema = new Schema({
 	name: { type: String, required: true, trim: true },
 	email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-	profile_picture : {type:String}
+	profile_picture : {type:String},
 	password: { type: String, required: true },
 	document: { type: String, trim: true },
 	address: { type: String, trim: true },
