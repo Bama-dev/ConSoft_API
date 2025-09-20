575b37ad561102c7c0518c13050b8cb885b6b5be
Juan Londoño <jelu0204@gmail.com>
2025-09-20 11:10:08 -0500

    Feat(Controller): Agregar pago del pedido
    - Se agrego la logica para calcular el estado del pago de un pedido
        para tener presente si ya esta pago en su totalidad o falta pago

--- src/controllers/order.Controller.ts
+++ src/controllers/order.Controller.ts
@@ -1,19 +1,34 @@
-import { Request, Response } from "express";
-import { OrderModel } from "../models/order.model";
-import { createCrudController } from "./crud.controller";
+import { Request, Response } from 'express';
+import { OrderModel } from '../models/order.model';
+import { createCrudController } from './crud.controller';
 
 const base = createCrudController(OrderModel);
 
 export const OrderController = {
-  ...base,
-  list: async (req: Request, res: Response) => {
-    try {
-      const orders = await OrderModel.find()
-        .populate("user", "-password -__v ")
-        .populate("payments");
-      res.json(orders);
-    } catch (error) {
-      res.status(500).json({ message: "Error retrieving orders" });
-    }
-  },
+	...base,
+	list: async (req: Request, res: Response) => {
+		try {
+			const orders = await OrderModel.find()
+				.populate('user', '-password -__v ')
+				.populate('payments');
+
+			const result = orders.map((order) => {
+				const total = order.items.reduce((sum, item) => sum + (item.valor || 0), 0);
+				const paid = order.payments.reduce((sum, p) => sum + (p.amount || 0), 0);
+				const restante = total - paid;
+
+				return {
+					...order.toObject(),
+					total,
+					paid,
+					restante,
+					paymentStatus: restante < 0 ? 'Pagado' : 'Pendiente',
+				};
+			});
+
+			res.json(orders);
+		} catch (error) {
+			res.status(500).json({ message: 'Error retrieving orders' });
+		}
+	},
 };
