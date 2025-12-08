bc2b36952f8ec82b35ba0b1e01b0f8d92dcb2bdb
= <=>
2025-12-08 15:48:23 -0500

    Update(cloud): Updated images folder name

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => ({
-    folder: "products",
+    folder: "ConSoft",
     resource_type: "image",
   }),
 });
